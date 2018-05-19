c92e7eaa0d819292d42f775d41e124539e6fcc3c
johnny huaman <johnny.ehl@gmail.com>
2018-05-19 15:47:44 -0500

    Primera version estable de proyecto facilito

---
+++ facilito/facilito/settings/base.py
@@ -0,0 +1,85 @@
+#import os
+from unipath import Path
+BASE_DIR = Path(__file__).ancestor(3)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'zv$#d#0ayobb=8oh+u%80vwz3&ggo8q#2+bnr#$c!o$6p*@3fp'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+DJANGO_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+LOCAL_APPS = (
+    'apps.facil',
+    'apps.users',
+)
+
+THIRD_PARTY_APPS = (
+
+)
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'facilito.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+WSGI_APPLICATION = 'facilito.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+AUTH_USER_MODEL = 'users.User'
---
+++ facilito/facilito/settings/local.py
@@ -0,0 +1,18 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'facilitodb',
+        'USER': 'faciluser',
+        'PASSWORD': 'jehl0205',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
